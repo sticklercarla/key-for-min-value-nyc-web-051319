ad16079b5bc192e33047c48781d500c98c4ab3a5
Carla Stickler <carlastickler@gmail.com>
2019-04-19 18:14:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -16,3 +16,18 @@
 #     min_key
 #   end
 # end
+def key_for_min_value(name_hash)
+  # code goes here
+  min_value = nil
+  min_key = nil
+  name_hash.each do |k, v|
+    if min_value == nil
+      min_value = v
+      min_key = k
+    elsif min_value > v
+      min_value = v
+      min_key = k
+    end
+  end
+  min_key
+end
