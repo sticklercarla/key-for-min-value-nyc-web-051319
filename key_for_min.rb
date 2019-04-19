9f2a55c98c720dfd15a0fb94e1062672d47aaf9d
Carla Stickler <carlastickler@gmail.com>
2019-04-19 18:10:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,7 @@ def key_for_min_value(name_hash)
     min_value = name_hash.first[1]
     min_key = name_hash.first[0]
     name_hash.each do |key, value|
-      
+      if value < min_value
+        
   
 end
